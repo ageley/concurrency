c98996d2a4b715ba9e273735247e70151560bea6
Andrei Gelei <a.geley@gmail.com>
2023-11-27 01:00:12 +0300

    fix: use AtomicInteger to calculate statistics

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -3,17 +3,18 @@ package course.concurrency.m3_shared.collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
         put("A", new Restaurant("A"));
         put("B", new Restaurant("B"));
         put("C", new Restaurant("C"));
     }};
 
-    private final Map<String, Integer> stat = new ConcurrentHashMap<>();
+    private final Map<String, AtomicInteger> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,7 +22,7 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        stat.merge(restaurantName, 1, Integer::sum);
+        stat.computeIfAbsent(restaurantName, name -> new AtomicInteger()).incrementAndGet();
     }
 
     public Set<String> printStat() {
