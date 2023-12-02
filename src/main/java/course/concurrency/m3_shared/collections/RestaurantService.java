161d570b43fd4ee5c6fa35514e68ae834d2080a1
Andrei Gelei <a.geley@gmail.com>
2023-12-02 18:38:24 +0300

    fix: add ReentrantReadWriteLock

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -4,17 +4,26 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
-        put("A", new Restaurant("A"));
-        put("B", new Restaurant("B"));
-        put("C", new Restaurant("C"));
-    }};
+    private final Map<String, Restaurant> restaurantMap;
+    private final Map<String, AtomicInteger> stat;
+    private final ReadWriteLock lock;
 
-    private final Map<String, AtomicInteger> stat = new ConcurrentHashMap<>();
+    public RestaurantService() {
+        this.restaurantMap = new ConcurrentHashMap() {{
+            put("A", new Restaurant("A"));
+            put("B", new Restaurant("B"));
+            put("C", new Restaurant("C"));
+        }};
+        this.stat = new ConcurrentHashMap<>();
+        this.lock = new ReentrantReadWriteLock();
+    }
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -22,13 +31,27 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        stat.computeIfAbsent(restaurantName, name -> new AtomicInteger()).incrementAndGet();
+        Lock operationalLock = lock.readLock();
+
+        try {
+            operationalLock.lock();
+            stat.computeIfAbsent(restaurantName, name -> new AtomicInteger()).incrementAndGet();
+        } finally {
+            operationalLock.unlock();
+        }
     }
 
     public Set<String> printStat() {
-        return stat.entrySet()
-                .stream()
-                .map(entry -> entry.getKey() + " - " + entry.getValue())
-                .collect(Collectors.toSet());
+        Lock reportingLock = lock.writeLock();
+
+        try {
+            reportingLock.lock();
+            return stat.entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + " - " + entry.getValue())
+                    .collect(Collectors.toSet());
+        } finally {
+            reportingLock.unlock();
+        }
     }
 }
