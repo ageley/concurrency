a568be2c4af40a05fe18a94738c86cecd8d16f99
Andrei Gelei <a.geley@gmail.com>
2023-12-17 16:06:12 +0300

    fix: use LongAdder

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -3,27 +3,18 @@ package course.concurrency.m3_shared.collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private final Map<String, Restaurant> restaurantMap;
-    private final Map<String, AtomicInteger> stat;
-    private final ReadWriteLock lock;
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+        put("A", new Restaurant("A"));
+        put("B", new Restaurant("B"));
+        put("C", new Restaurant("C"));
+    }};
 
-    public RestaurantService() {
-        this.restaurantMap = new ConcurrentHashMap() {{
-            put("A", new Restaurant("A"));
-            put("B", new Restaurant("B"));
-            put("C", new Restaurant("C"));
-        }};
-        this.stat = new ConcurrentHashMap<>();
-        this.lock = new ReentrantReadWriteLock();
-    }
+    private final Map<String, LongAdder> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -31,27 +22,13 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        Lock operationalLock = lock.readLock();
-
-        try {
-            operationalLock.lock();
-            stat.computeIfAbsent(restaurantName, name -> new AtomicInteger()).incrementAndGet();
-        } finally {
-            operationalLock.unlock();
-        }
+        stat.computeIfAbsent(restaurantName, name -> new LongAdder()).increment();
     }
 
     public Set<String> printStat() {
-        Lock reportingLock = lock.writeLock();
-
-        try {
-            reportingLock.lock();
-            return stat.entrySet()
-                    .stream()
-                    .map(entry -> entry.getKey() + " - " + entry.getValue())
-                    .collect(Collectors.toSet());
-        } finally {
-            reportingLock.unlock();
-        }
+        return stat.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue().sum())
+                .collect(Collectors.toSet());
     }
 }
